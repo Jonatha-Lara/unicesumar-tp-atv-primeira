ade5d761030283bb5561358068dbd4ff0ff4df2d
Jonatha Lara <jonathalarasilverio@gmail.com>
2023-04-21 00:51:41 -0300

    hello word

---
+++ topicosEspeciais/src/main/java/com/topicosespeciais/topicosEspeciais/app/TopicosEspeciaisApplication.java
@@ -0,0 +1,18 @@
+package com.topicosespeciais.topicosEspeciais.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicosEspeciaisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopicosEspeciaisApplication.class, args);
+		
+		System.out.println("Hello word!");
+		System.out.println("Hello word!");
+		System.out.println("Hello word!");
+		System.out.println("Hello word!");
+	}
+
+}
---
+++ topicosEspeciais/src/test/java/com/topicosespeciais/topicosEspeciais/TopicosEspeciaisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topicosespeciais.topicosEspeciais;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopicosEspeciaisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
